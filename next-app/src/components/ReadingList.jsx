fc96cbc9d38a806593307e9b16a2ccdd71e3308d
Ricardo Munda <rjmunda21@gmail.com>
2024-05-12 14:57:39 -0500

    Updated front-end

--- next-app/src/components/ReadingList.jsx
+++ next-app/src/components/ReadingList.jsx
@@ -1,12 +1,18 @@
+import { useEffect, useState } from "react";
 import MeterGauge from "./Meter";
 import MeterCard from "./MeterCard";
 import MyReadingsCard from "./ReadingCard";
+import { APIEndpoint } from "@/utils/api";
 
 const ReadingList = () => {
+
+    const farmAPI = new APIEndpoint('...')
+    const [data, setData] = useState({})
+
     return ( 
         <div className="flex flex-col gap-6 flex-wrap">
             <div className="flex flex-row gap-6 flex-wrap">
-                <MyReadingsCard heading={"Humidity(%)"} reading={"34.5"} image_src={"/images/humidity.png"}/>
+                <MyReadingsCard heading={"Humidity(%)"} reading={data.humidity} image_src={"/images/humidity.png"}/>
                 <MyReadingsCard heading={"Temperature(°C)"} reading={"23.5"} image_src={"/images/temperature.png"}/>
                 <MyReadingsCard heading={"Heat Index(°C)"} reading={"34.5"} image_src={"/images/heat_index.png"}/>
                 <MyReadingsCard heading={"Air Pressure(Pa)"} reading={"93234.5"} image_src={"/images/air_pressure.png"}/>
