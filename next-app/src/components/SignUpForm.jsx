2df954ea4f4f90c5fd671fecba264f9950d25bb9
Ricardo Munda <rjmunda21@gmail.com>
2024-05-13 00:46:27 -0500

    Updated front-end

--- next-app/src/components/SignUpForm.jsx
+++ next-app/src/components/SignUpForm.jsx
@@ -1,12 +1,12 @@
-'use client'
+"use client";
 
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 // Components Import
-import { Input } from './ui/input';
-import { Button } from "@/components/ui/button"
+import { Input } from "./ui/input";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -15,109 +15,151 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { APIEndpoint } from '@/utils/api';
+} from "@/components/ui/form";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+import { APIEndpoint } from "@/utils/api";
 
 const SignUpForm = () => {
 
-    const formSchema = z.object({
-        firstname: z.string().min(1, {
-            message: "This field is required.",
-        }),
-        lastname: z.string().min(1, {
-            message: "This field is required.",
-        }),
-        username: z.string().min(1, {
-            message: "This field is required.",
-        }),
-        password: z.string().min(1,{
-            message: "This field is required."
-        }),
-    })
+    const CROP_TYPES = ['Rice', 'Maize', 'Chickpea', 'Kidneybeans', 'Pigeonpeas',
+    'Mothbeans', 'Mungbean', 'Blackgram', 'Lentil', 'Pomegranate',
+    'Banana', 'Mango', 'Grapes', 'Watermelon', 'Muskmelon', 'Apple',
+    'Orange', 'Papaya', 'Coconut', 'Cotton', 'Jute', 'Coffee']
 
-    const form = useForm({
-        resolver: zodResolver(formSchema),
-        defaultValues:{
-            firstname: '',
-            lastname: '',
-            username: '',
-            password: ''
-        }
-    })
+  const formSchema = z.object({
+    firstname: z.string().min(1, {
+      message: "This field is required.",
+    }),
+    lastname: z.string().min(1, {
+      message: "This field is required.",
+    }),
+    croptype: z.string().min(1, {
+      message: "This field is required.",
+    }),
+    username: z.string().min(1, {
+      message: "This field is required.",
+    }),
+    password: z.string().min(1, {
+      message: "This field is required.",
+    }),
+  });
 
-    const onSubmit = (data) => {
-        const registerAPI = new APIEndpoint()
-        registerAPI.post('register/', data)
-        .then((res)=>{
-            console.log(res)
-        })
-        .catch(err=>console.log(err))
-        console.log(data)
-    }
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstname: "",
+      lastname: "",
+      username: "",
+      password: "",
+      croptype: "",
+    },
+  });
 
-    return ( 
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+  const onSubmit = (data) => {
+    const registerAPI = new APIEndpoint();
+    registerAPI
+      .post("register/", data)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+    console.log(data);
+  };
 
-                <div className='flex flex-row gap-5'>
-                    <FormField
-                    
-                    control={form.control}
-                    name="firstname"
-                    render={({ field }) => (
-                        <FormItem className='flex-grow'>
-                        <FormLabel>First Name</FormLabel>
-                        <FormControl>
-                            <Input placeholder='First Name' {...field} />
-                        </FormControl>
-                        <FormMessage />
-                        </FormItem>
-                    )}
-                    />
-                    <FormField
-                    control={form.control}
-                    name="lastname"
-                    render={({ field }) => (
-                        <FormItem className='flex-grow'>
-                        <FormLabel>Last Name</FormLabel>
-                        <FormControl>
-                            <Input placeholder='Last Name' {...field} />
-                        </FormControl>
-                        <FormMessage />
-                        </FormItem>
-                    )}
-                    />
-                </div>
-                <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                    <FormItem>
-                    <FormLabel>Username</FormLabel>
-                    <FormControl>
-                        <Input placeholder="Username" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                    </FormItem>
-                )}
-                />
-                <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                    <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                        <Input placeholder="Password" type='password' {...field} />
-                    </FormControl>
-                    <FormMessage />
-                    </FormItem>
-                )}
-                />
-                <Button className="w-full" type="submit">Sign Up</Button>
-            </form>
-        </Form>
-     );
-}
- 
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <div className="flex flex-row gap-5">
+          <FormField
+            control={form.control}
+            name="firstname"
+            render={({ field }) => (
+              <FormItem className="flex-grow">
+                <FormLabel>First Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="First Name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="lastname"
+            render={({ field }) => (
+              <FormItem className="flex-grow">
+                <FormLabel>Last Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Last Name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="Username" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="croptype"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Crop</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select a crop type" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                    {CROP_TYPES.map(croptype=>(
+                        <SelectItem value={croptype}>{croptype}</SelectItem>
+                    ))}
+                </SelectContent>
+              </Select>
+              <FormDescription>
+                You can change your crop type later.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="Password" type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button className="w-full" type="submit">
+          Sign Up
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
 export default SignUpForm;
