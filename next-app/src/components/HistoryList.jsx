83bc1de02993d21ca24e5e6a5e572e1f36c56410
Ricardo Munda <rjmunda21@gmail.com>
2024-05-14 14:03:19 -0500

    Updated frontend history

--- next-app/src/components/HistoryList.jsx
+++ next-app/src/components/HistoryList.jsx
@@ -8,89 +8,9 @@ import dynamic from "next/dynamic";
 
 const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
-const initialChartConfig = {
-  type: "line",
-  height: 240,
-  width: "100%",
-  series: [
-    {
-      name: "Humidity",
-      data: [],
-    },
-  ],
-  options: {
-    chart: {
-      toolbar: {
-        show: false,
-      },
-    },
-    title: {
-      show: "",
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    colors: ["#1ecbe1"],
-    stroke: {
-      lineCap: "round",
-      curve: "smooth",
-    },
-    markers: {
-      size: 0,
-    },
-    xaxis: {
-      axisTicks: {
-        show: false,
-      },
-      axisBorder: {
-        show: false,
-      },
-      labels: {
-        style: {
-          colors: "#616161",
-          fontSize: "12px",
-          fontFamily: "inherit",
-          fontWeight: 400,
-        },
-      },
-      categories: ["Mon", "Tue", "Wed", "Thr", "Fri", "Sat", "Sun"],
-    },
-    yaxis: {
-      labels: {
-        style: {
-          colors: "#616161",
-          fontSize: "12px",
-          fontFamily: "inherit",
-          fontWeight: 400,
-        },
-      },
-    },
-    grid: {
-      show: true,
-      borderColor: "#dddddd",
-      strokeDashArray: 5,
-      xaxis: {
-        lines: {
-          show: true,
-        },
-      },
-      padding: {
-        top: 5,
-        right: 20,
-      },
-    },
-    fill: {
-      opacity: 0.8,
-    },
-    tooltip: {
-      theme: "dark",
-    },
-  },
-};
-
 const HistoryList = () => {
   const { startOfWeek, endOfWeek } = getStartAndEndOfWeek();
-  const [humidityConfig, setHumidityConfig] = useState(initialChartConfig);
+  const [humidityValues, setHumidityValues] = useState([])
 
   useEffect(() => {
     fetchHistory();
@@ -102,12 +22,7 @@ const HistoryList = () => {
       .then((res) => {
         if (res.data) {
           const values = res.data.map(obj => Number(obj.value.toFixed(2)));
-          console.log("Fetched values:", values);
-
-          setHumidityConfig(prevConfig => ({
-            ...prevConfig,
-            series: [{ ...prevConfig.series[0], data: values }],
-          }));
+          setHumidityValues(values)
         }
       })
       .catch(err => console.log(err));
@@ -116,19 +31,98 @@ const HistoryList = () => {
   return (
     <div className="w-full flex flex-col gap-5">
       <div className="flex flex-row gap-5">
-        <LineChart config={humidityConfig} title={'Humidity(%)'} />
+        <LineChart values={humidityValues ? humidityValues : []} title={'Humidity(%)'} />
       </div>
     </div>
   );
 };
 
-const LineChart = ({ title, config }) => {
+const LineChart = ({ title, values }) => {
   return (
     <Card className="w-[100%]">
       <h1 className="font-bold text-lg">{title}</h1>
-      <Chart key={JSON.stringify(config.series[0].data)} {...config} />
+      <Chart
+        type={"line"}
+        height={240}
+        width={"100%"}
+        series={[
+          {
+            name: {title},
+            data: values,
+          },
+        ]}
+        options= {{
+          chart: {
+            toolbar: {
+              show: false,
+            },
+          },
+          title: {
+            show: "",
+          },
+          dataLabels: {
+            enabled: false,
+          },
+          colors: ["#1ecbe1"],
+          stroke: {
+            lineCap: "round",
+            curve: "smooth",
+          },
+          markers: {
+            size: 0,
+          },
+          xaxis: {
+            axisTicks: {
+              show: false,
+            },
+            axisBorder: {
+              show: false,
+            },
+            labels: {
+              style: {
+                colors: "#616161",
+                fontSize: "12px",
+                fontFamily: "inherit",
+                fontWeight: 400,
+              },
+            },
+            categories: ["Mon", "Tue", "Wed", "Thr", "Fri", "Sat", "Sun"],
+          },
+          yaxis: {
+            labels: {
+              style: {
+                colors: "#616161",
+                fontSize: "12px",
+                fontFamily: "inherit",
+                fontWeight: 400,
+              },
+            },
+          },
+          grid: {
+            show: true,
+            borderColor: "#dddddd",
+            strokeDashArray: 5,
+            xaxis: {
+              lines: {
+                show: true,
+              },
+            },
+            padding: {
+              top: 5,
+              right: 20,
+            },
+          },
+          fill: {
+            opacity: 0.8,
+          },
+          tooltip: {
+            theme: "dark",
+          }
+        }}
+      />
+      {/* <Chart key={JSON.stringify(config.series[0].data)} {...config} /> */}
     </Card>
   );
-};
+}
 
 export default HistoryList;
