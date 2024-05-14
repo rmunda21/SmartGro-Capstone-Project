141200f6b1c1fc89085686d1cee35201ed9f7f02
Ricardo Munda <rjmunda21@gmail.com>
2024-05-14 13:09:56 -0500

    Updated frontend history

--- next-app/src/components/HistoryList.jsx
+++ next-app/src/components/HistoryList.jsx
@@ -5,7 +5,7 @@ import LineChart from "./LineChart";
 import { getStartAndEndOfWeek } from "@/utils/helper";
 import { useEffect, useState } from "react";
 
-const HumidityChartConfig = {
+const initialHumidityChartConfig = {
   type: "line",
   height: 240,
   width: "100%",
@@ -97,15 +97,13 @@ const HistoryList = () => {
     const { startOfWeek, endOfWeek } = getStartAndEndOfWeek()
 
     const [humidityValues, setHumidityValues] = useState([])
-    const [humidityConfig, setHumidityConfig] = useState(HumidityChartConfig)
+    const [humidityConfig, setHumidityConfig] = useState(initialHumidityChartConfig)
 
     useEffect(()=>{
       fetchHistory()
     }, [])
 
     const fetchHistory = () =>{
-      
-      // console.log(startOfWeek, endOfWeek)
       const historyAPI = new APIEndpoint()
       historyAPI.get(`graph/${startOfWeek}/${endOfWeek}/Humidity`)
       .then((res)=>{
@@ -118,372 +116,369 @@ const HistoryList = () => {
             const newConfig = { ...prevConfig };
             newConfig.series = [{ ...newConfig.series[0], data: t }];
             return newConfig;
-        });
+          });
           console.log(humidityConfig)
-      }
-
+        }
       })
-      .catch(err=>console.log(err))
+      .catch(err => console.log(err))
     }
 
-   
-
     const AirQualityChartConfig = {
-    type: "line",
-    height: 240,
-    width: "100%",
-    series: [
-        {
-        name: "Air Quality",
-        data: [0, 50, 100, 150, 200, 300, 500],
-        },
-    ],
-    options: {
-        chart: {
-        toolbar: {
-            show: false,
-        },
-        },
-        title: {
-        show: "",
-        },
-        dataLabels: {
-        enabled: false,
-        },
-        colors: ["#84e1a0"],
-        stroke: {
-        lineCap: "round",
-        curve: "smooth",
-        },
-        markers: {
-        size: 0,
-        },
-        xaxis: {
-        axisTicks: {
-            show: false,
-        },
-        axisBorder: {
-            show: false,
-        },
-        labels: {
-            style: {
-            colors: "#616161",
-            fontSize: "12px",
-            fontFamily: "inherit",
-            fontWeight: 400,
+        type: "line",
+        height: 240,
+        width: "100%",
+        series: [
+            {
+            name: "Air Quality",
+            data: [0, 50, 100, 150, 200, 300, 500],
             },
-        },
-        categories: [
-            "Mon",
-            "Tue",
-            "Wed",
-            "Thr",
-            "Fri",
-            "Sat",
-            "Sun",
         ],
-        },
-        yaxis: {
-        labels: {
-            style: {
-            colors: "#616161",
-            fontSize: "12px",
-            fontFamily: "inherit",
-            fontWeight: 400,
+        options: {
+            chart: {
+            toolbar: {
+                show: false,
             },
-        },
-        },
-        grid: {
-        show: true,
-        borderColor: "#dddddd",
-        strokeDashArray: 5,
-        xaxis: {
-            lines: {
+            },
+            title: {
+            show: "",
+            },
+            dataLabels: {
+            enabled: false,
+            },
+            colors: ["#84e1a0"],
+            stroke: {
+            lineCap: "round",
+            curve: "smooth",
+            },
+            markers: {
+            size: 0,
+            },
+            xaxis: {
+            axisTicks: {
+                show: false,
+            },
+            axisBorder: {
+                show: false,
+            },
+            labels: {
+                style: {
+                colors: "#616161",
+                fontSize: "12px",
+                fontFamily: "inherit",
+                fontWeight: 400,
+                },
+            },
+            categories: [
+                "Mon",
+                "Tue",
+                "Wed",
+                "Thr",
+                "Fri",
+                "Sat",
+                "Sun",
+            ],
+            },
+            yaxis: {
+            labels: {
+                style: {
+                colors: "#616161",
+                fontSize: "12px",
+                fontFamily: "inherit",
+                fontWeight: 400,
+                },
+            },
+            },
+            grid: {
             show: true,
+            borderColor: "#dddddd",
+            strokeDashArray: 5,
+            xaxis: {
+                lines: {
+                show: true,
+                },
+            },
+            padding: {
+                top: 5,
+                right: 20,
+            },
+            },
+            fill: {
+            opacity: 0.8,
+            },
+            tooltip: {
+            theme: "dark",
             },
         },
-        padding: {
-            top: 5,
-            right: 20,
-        },
-        },
-        fill: {
-        opacity: 0.8,
-        },
-        tooltip: {
-        theme: "dark",
-        },
-    },
     };
 
     const TemperatureChartConfig = {
-    type: "bar",
-    height: 240,
-    width: "100%",
-    series: [
-        {
-        name: "Temperature",
-        data: [20, 25, 30, 35, 40],
-        },
-    ],
-    options: {
-        chart: {
-        toolbar: {
-            show: false,
-        },
-        },
-        title: {
-        show: "",
-        },
-        dataLabels: {
-        enabled: false,
-        },
-        colors: ["#f3f989"],
-        stroke: {
-        lineCap: "round",
-        curve: "smooth",
-        },
-        markers: {
-        size: 0,
-        },
-        xaxis: {
-        axisTicks: {
-            show: false,
-        },
-        axisBorder: {
-            show: false,
-        },
-        labels: {
-            style: {
-            colors: "#616161",
-            fontSize: "12px",
-            fontFamily: "inherit",
-            fontWeight: 400,
+        type: "bar",
+        height: 240,
+        width: "100%",
+        series: [
+            {
+            name: "Temperature",
+            data: [20, 25, 30, 35, 40],
             },
-        },
-        categories: [
-            "Mon",
-            "Tue",
-            "Wed",
-            "Thr",
-            "Fri",
-            "Sat",
-            "Sun",
         ],
-        },
-        yaxis: {
-        labels: {
-            style: {
-            colors: "#616161",
-            fontSize: "12px",
-            fontFamily: "inherit",
-            fontWeight: 400,
+        options: {
+            chart: {
+            toolbar: {
+                show: false,
             },
-        },
-        },
-        grid: {
-        show: true,
-        borderColor: "#dddddd",
-        strokeDashArray: 5,
-        xaxis: {
-            lines: {
+            },
+            title: {
+            show: "",
+            },
+            dataLabels: {
+            enabled: false,
+            },
+            colors: ["#f3f989"],
+            stroke: {
+            lineCap: "round",
+            curve: "smooth",
+            },
+            markers: {
+            size: 0,
+            },
+            xaxis: {
+            axisTicks: {
+                show: false,
+            },
+            axisBorder: {
+                show: false,
+            },
+            labels: {
+                style: {
+                colors: "#616161",
+                fontSize: "12px",
+                fontFamily: "inherit",
+                fontWeight: 400,
+                },
+            },
+            categories: [
+                "Mon",
+                "Tue",
+                "Wed",
+                "Thr",
+                "Fri",
+                "Sat",
+                "Sun",
+            ],
+            },
+            yaxis: {
+            labels: {
+                style: {
+                colors: "#616161",
+                fontSize: "12px",
+                fontFamily: "inherit",
+                fontWeight: 400,
+                },
+            },
+            },
+            grid: {
             show: true,
+            borderColor: "#dddddd",
+            strokeDashArray: 5,
+            xaxis: {
+                lines: {
+                show: true,
+                },
+            },
+            padding: {
+                top: 5,
+                right: 20,
+            },
+            },
+            fill: {
+            opacity: 0.8,
+            },
+            tooltip: {
+            theme: "dark",
             },
         },
-        padding: {
-            top: 5,
-            right: 20,
-        },
-        },
-        fill: {
-        opacity: 0.8,
-        },
-        tooltip: {
-        theme: "dark",
-        },
-    },
     };
 
     const HeatIndexChartConfig = {
-    type: "bar",
-    height: 240,
-    width: "100%",
-    series: [
-        {
-        name: "Heat Index",
-        data: [20, 25, 30, 35, 40],
-        },
-    ],
-    options: {
-        chart: {
-        toolbar: {
-            show: false,
-        },
-        },
-        title: {
-        show: "",
-        },
-        dataLabels: {
-        enabled: false,
-        },
-        colors: ["#f45b56"],
-        stroke: {
-        lineCap: "round",
-        curve: "smooth",
-        },
-        markers: {
-        size: 0,
-        },
-        xaxis: {
-        axisTicks: {
-            show: false,
-        },
-        axisBorder: {
-            show: false,
-        },
-        labels: {
-            style: {
-            colors: "#616161",
-            fontSize: "12px",
-            fontFamily: "inherit",
-            fontWeight: 400,
+        type: "bar",
+        height: 240,
+        width: "100%",
+        series: [
+            {
+            name: "Heat Index",
+            data: [20, 25, 30, 35, 40],
             },
-        },
-        categories: [
-            "Mon",
-            "Tue",
-            "Wed",
-            "Thr",
-            "Fri",
-            "Sat",
-            "Sun",
         ],
-        },
-        yaxis: {
-        labels: {
-            style: {
-            colors: "#616161",
-            fontSize: "12px",
-            fontFamily: "inherit",
-            fontWeight: 400,
+        options: {
+            chart: {
+            toolbar: {
+                show: false,
             },
-        },
-        },
-        grid: {
-        show: true,
-        borderColor: "#dddddd",
-        strokeDashArray: 5,
-        xaxis: {
-            lines: {
+            },
+            title: {
+            show: "",
+            },
+            dataLabels: {
+            enabled: false,
+            },
+            colors: ["#f45b56"],
+            stroke: {
+            lineCap: "round",
+            curve: "smooth",
+            },
+            markers: {
+            size: 0,
+            },
+            xaxis: {
+            axisTicks: {
+                show: false,
+            },
+            axisBorder: {
+                show: false,
+            },
+            labels: {
+                style: {
+                colors: "#616161",
+                fontSize: "12px",
+                fontFamily: "inherit",
+                fontWeight: 400,
+                },
+            },
+            categories: [
+                "Mon",
+                "Tue",
+                "Wed",
+                "Thr",
+                "Fri",
+                "Sat",
+                "Sun",
+            ],
+            },
+            yaxis: {
+            labels: {
+                style: {
+                colors: "#616161",
+                fontSize: "12px",
+                fontFamily: "inherit",
+                fontWeight: 400,
+                },
+            },
+            },
+            grid: {
             show: true,
+            borderColor: "#dddddd",
+            strokeDashArray: 5,
+            xaxis: {
+                lines: {
+                show: true,
+                },
+            },
+            padding: {
+                top: 5,
+                right: 20,
+            },
+            },
+            fill: {
+            opacity: 0.8,
+            },
+            tooltip: {
+            theme: "dark",
             },
         },
-        padding: {
-            top: 5,
-            right: 20,
-        },
-        },
-        fill: {
-        opacity: 0.8,
-        },
-        tooltip: {
-        theme: "dark",
-        },
-    },
     };
 
     const SoilMoistureChartConfig = {
-    type: "line",
-    height: 240,
-    width: "100%",
-    series: [
-        {
-        name: "Soil Moisture",
-        data: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
-        },
-    ],
-    options: {
-        chart: {
-        toolbar: {
-            show: false,
-        },
-        },
-        title: {
-        show: "",
-        },
-        dataLabels: {
-        enabled: false,
-        },
-        colors: ["#e0823b"],
-        stroke: {
-        lineCap: "round",
-        curve: "smooth",
-        },
-        markers: {
-        size: 0,
-        },
-        xaxis: {
-        axisTicks: {
-            show: false,
-        },
-        axisBorder: {
-            show: false,
-        },
-        labels: {
-            style: {
-            colors: "#616161",
-            fontSize: "12px",
-            fontFamily: "inherit",
-            fontWeight: 400,
+        type: "line",
+        height: 240,
+        width: "100%",
+        series: [
+            {
+            name: "Soil Moisture",
+            data: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
             },
-        },
-        categories: [
-            "Mon",
-            "Tue",
-            "Wed",
-            "Thr",
-            "Fri",
-            "Sat",
-            "Sun",
         ],
-        },
-        yaxis: {
-        labels: {
-            style: {
-            colors: "#616161",
-            fontSize: "12px",
-            fontFamily: "inherit",
-            fontWeight: 400,
+        options: {
+            chart: {
+            toolbar: {
+                show: false,
             },
-        },
-        },
-        grid: {
-        show: true,
-        borderColor: "#dddddd",
-        strokeDashArray: 5,
-        xaxis: {
-            lines: {
+            },
+            title: {
+            show: "",
+            },
+            dataLabels: {
+            enabled: false,
+            },
+            colors: ["#e0823b"],
+            stroke: {
+            lineCap: "round",
+            curve: "smooth",
+            },
+            markers: {
+            size: 0,
+            },
+            xaxis: {
+            axisTicks: {
+                show: false,
+            },
+            axisBorder: {
+                show: false,
+            },
+            labels: {
+                style: {
+                colors: "#616161",
+                fontSize: "12px",
+                fontFamily: "inherit",
+                fontWeight: 400,
+                },
+            },
+            categories: [
+                "Mon",
+                "Tue",
+                "Wed",
+                "Thr",
+                "Fri",
+                "Sat",
+                "Sun",
+            ],
+            },
+            yaxis: {
+            labels: {
+                style: {
+                colors: "#616161",
+                fontSize: "12px",
+                fontFamily: "inherit",
+                fontWeight: 400,
+                },
+            },
+            },
+            grid: {
             show: true,
+            borderColor: "#dddddd",
+            strokeDashArray: 5,
+            xaxis: {
+                lines: {
+                show: true,
+                },
+            },
+            padding: {
+                top: 5,
+                right: 20,
+            },
+            },
+            fill: {
+            opacity: 0.8,
+            },
+            tooltip: {
+            theme: "dark",
             },
         },
-        padding: {
-            top: 5,
-            right: 20,
-        },
-        },
-        fill: {
-        opacity: 0.8,
-        },
-        tooltip: {
-        theme: "dark",
-        },
-    },
     };
 
     return ( 
         <div className="w-full flex flex-col gap-5">
             <div className="flex flex-row gap-5">
-                <LineChart config={HumidityChartConfig} title={'Humidity(%)'} />
+                <LineChart config={humidityConfig} title={'Humidity(%)'} />
                 <LineChart config={TemperatureChartConfig} title={'Temperature(°C)'} />
             </div>
             <div className="w-full flex flex-col">
