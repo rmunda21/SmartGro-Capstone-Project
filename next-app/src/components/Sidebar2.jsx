1889e8f4da74121dc4d08fed9006b8531e810ad9
Ricardo Munda <rjmunda21@gmail.com>
2024-05-13 21:24:23 -0500

    Updated frontend home and live readings

--- next-app/src/components/Sidebar2.jsx
+++ next-app/src/components/Sidebar2.jsx
@@ -1,37 +1,36 @@
-'use client'
+"use client";
 
 import {
-    Card,
-    Typography,
-    List,
-    ListItem,
-    ListItemPrefix,
-    ListItemSuffix,
-    Chip,
-  } from "@material-tailwind/react";
-  import {
-    TableCellsIcon,
-    InformationCircleIcon,
-    ChartBarIcon,
-    HomeIcon,
-    PowerIcon
-  } from "@heroicons/react/24/solid";
-  import Link from 'next/link'
+  Card,
+  Typography,
+  List,
+  ListItem,
+  ListItemPrefix,
+  ListItemSuffix,
+  Chip,
+} from "@material-tailwind/react";
+import {
+  TableCellsIcon,
+  InformationCircleIcon,
+  ChartBarIcon,
+  HomeIcon,
+  PowerIcon,
+} from "@heroicons/react/24/solid";
+import Link from "next/link";
 import { APIEndpoint } from "@/utils/api";
 import { useRouter } from "next/navigation";
-   
-  export function DefaultSidebar() {
 
-    const router = useRouter()
+export function DefaultSidebar() {
+  const router = useRouter();
 
-    return (
-      <Card className="h-screen w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
-        <div className="mb-2 p-4">
-          <Typography variant="h5" color="blue-gray">
-            SmartGro
-          </Typography>
-        </div>
-        <List>
+  return (
+    <Card className="h-screen w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
+      <div className="mb-2 p-4">
+        <Typography variant="h5" color="blue-gray">
+          SmartGro
+        </Typography>
+      </div>
+      <List>
         <Link href={"/dashboard/home"}>
           <ListItem>
             <ListItemPrefix>
@@ -39,52 +38,55 @@ import { useRouter } from "next/navigation";
             </ListItemPrefix>
             Home
           </ListItem>
-          </Link>
-          <Link href={"/dashboard/live-readings"}>
+        </Link>
+        <Link href={"/dashboard/live-readings"}>
           <ListItem>
             <ListItemPrefix>
               <TableCellsIcon className="h-5 w-5" />
             </ListItemPrefix>
             Live Readings
           </ListItem>
-          </Link>
-          <Link href={"/dashboard/history"}>
+        </Link>
+        <Link href={"/dashboard/history"}>
           <ListItem>
             <ListItemPrefix>
               <ChartBarIcon className="h-5 w-5" />
             </ListItemPrefix>
             History
           </ListItem>
-          </Link>
-          <Link href={"/dashboard/about"}>
+        </Link>
+        <Link href={"/dashboard/about"}>
           <ListItem>
             <ListItemPrefix>
               <InformationCircleIcon className="h-5 w-5" />
             </ListItemPrefix>
             About
           </ListItem>
-          </Link>
-          {/* <ListItem>
+        </Link>
+        {/* <ListItem>
             <ListItemPrefix>
               <Cog6ToothIcon className="h-5 w-5" />
             </ListItemPrefix>
             Settings
           </ListItem> */}
-          <ListItem onClick={()=>{
-            const logoutAPI = new APIEndpoint()
-            logoutAPI.post('logout/')
-            .then((res)=>{
-              console.log(res)
-              router.push('/sign-in')
-            })
-            .catch(err=>console.log(err))
-          }}>
-            <ListItemPrefix>
-              <PowerIcon className="h-5 w-5" />
-            </ListItemPrefix>
-            Log Out
-          </ListItem>
-        </List>
-      </Card>
-    );
-  }
+        <ListItem
+          onClick={() => {
+            const logoutAPI = new APIEndpoint();
+            logoutAPI
+              .post("logout/")
+              .then((res) => {
+                console.log(res);
+                router.push("/sign-in");
+              })
+              .catch((err) => console.log(err));
+          }}
+        >
+          <ListItemPrefix>
+            <PowerIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Log Out
+        </ListItem>
+      </List>
+    </Card>
+  );
+}
